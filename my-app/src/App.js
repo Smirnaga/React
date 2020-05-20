f1f1c782b46cea064c4128c220324e67f58ed7b4
Smirnaga <smirnagakirill@gmail.com>
2020-05-20 17:24:31 +0300

    hw

---
+++ my-app/src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import myPhoto from './myPhoto.jpg';
+import './App.css';
+
+export default class App extends Component  {
+  render() {
+    return (
+      <div className="App">
+      <header className="App-header">
+        <img src={myPhoto} className="App-logo" alt="logo" />
+        <p>
+        <h2>Hi, I'm <span>Kirill</span> . I start learning React. This is my first homework on this course. A little bit about yourself. I love sports, but there is no time yet, since quarantine and children are at home) I like new technologies, films  and delicious food  =)</h2>
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React   
+        </a>
+      </header>
+    </div>
+    )
+  }
+}
