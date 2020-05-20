ea1e1ec61f86a5953b6c80f13c08fbe2dc51a2e8
Smirnaga <smirnagakirill@gmail.com>
2020-05-20 17:29:32 +0300

    hw1

--- HomeWork1/my-app/src/App.js
+++ HomeWork1/my-app/src/App.js
@@ -9,7 +9,7 @@ export default class App extends Component  {
       <header className="App-header">
         <img src={myPhoto} className="App-logo" alt="logo" />
         <p>
-        <h2>Hi, I'm <span>Kirill</span> . I start learning React. This is my first homework on this course. A little bit about yourself. I love sports, but there is no time yet, since quarantine and children are at home) I like new technologies, films  and delicious food  =)</h2>
+        <h2>Hi, I'm <span>Kirill</span> . I start learning React. This is my first homework on this course. A little bit about yourself. I love sports, but  there is no time yet, since quarantine and children are at home) I like new technologies, films  and delicious food  =)</h2>
         </p>
         <a
           className="App-link"
